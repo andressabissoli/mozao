d2ff10a652092cdf824e7828881d292f96b08d20
Andressa Bissoli <andressapbissoli@gmail.com>
2025-08-05 13:03:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function check() {
+  const msg = document.getElementById("mensagem");
+
   if (
-    document.forms[0].elements[0].checked == true &&
-    document.forms[0].elements[1].checked == true &&
-    document.forms[0].elements[2].checked == true
+    document.forms[0].elements[0].checked === true &&
+    document.forms[0].elements[1].checked === true &&
+    document.forms[0].elements[2].checked === true
   ) {
     if (!$('.wrapper').hasClass('throb')) {
       $('.wrapper').addClass('throb');
     }
+
+    // Mostrar a mensagem
+    msg.style.display = "block";
+
   } else {
     if ($('.wrapper').hasClass('throb')) {
       $('.wrapper').removeClass('throb');
     }
+
+    // Esconder a mensagem
+    msg.style.display = "none";
   }
 }
