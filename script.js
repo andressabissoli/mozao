554f0d76aa8e9378411cf373176818fa30f868db
Andressa Bissoli <andressapbissoli@gmail.com>
2025-08-05 12:52:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function check() {
+  if (
+    document.forms[0].elements[0].checked == true &&
+    document.forms[0].elements[1].checked == true &&
+    document.forms[0].elements[2].checked == true
+  ) {
+    if (!$('.wrapper').hasClass('throb')) {
+      $('.wrapper').addClass('throb');
+    }
+  } else {
+    if ($('.wrapper').hasClass('throb')) {
+      $('.wrapper').removeClass('throb');
+    }
+  }
+}
